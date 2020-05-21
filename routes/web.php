0b3d056b6f6ccd837d266e03614aafa7a63aecb1
Nelaka Jayasinghe <it18067552@my.sliit.lk>
2020-05-21 12:33:25 +0530

    First Commit

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+//Route::
+Route::get('/dataEntry', function () {
     return view('welcome');
 });
+
+Route::get('/Test', function () {
+    return view('welcome');
+});
+
