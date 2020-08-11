df0049476a9b6d1a78d94048f3a95b7be3b4bc51
Rishikeish <20205306@student.curtin.edu.au>
2020-08-11 23:13:03 +0530

    Migration error resolved

--- database/migrations/2014_07_07_000000_create_users_table.php
+++ database/migrations/2014_07_07_000000_create_users_table.php
@@ -16,14 +16,14 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('farmer_id');
-            // $table->string('name');
-            // $table->string('email')->unique();
-            // $table->timestamp('email_verified_at')->nullable();
-            // $table->string('password');
-            // $table->rememberToken();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
             $table->timestamps();
-           // $table->foreign('farmer_id')->references('id')->on('farmers');
         });
+
     }
 
     /**
