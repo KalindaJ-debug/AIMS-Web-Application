bee8c5d339bf12948330925f388eccb0dccd8fac
Indumini Ranasinghe <20205283@student.curtin.edu.au>
2020-08-16 00:01:02 +0530

    Land Controller

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\RegistrationController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -35,6 +36,7 @@ Route::get('/adminharvest', 'PagesController@adminharvest');
 Route::resource('approval', 'ApprovalController'); 
 Route::resource('registration', 'RegistrationController'); 
 Route::resource('crop', 'CropController');
+Route::resource('land', 'LandController');
 
 Route::get('/home', function () {
     return view('home');
@@ -99,13 +101,10 @@ Route::get('/croplist', function () {
     return view('croplist');
 });
 
-<<<<<<< HEAD
-
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-=======
 Route::get('/land-records', function () {
     return view('land-records');
 });
@@ -113,4 +112,3 @@ Route::get('/land-records', function () {
 Route::get('/land-update', function () {
     return view('land-record-update');
 });
->>>>>>> 0313731b2498f727f7974ab6cf78b43b028ca945
