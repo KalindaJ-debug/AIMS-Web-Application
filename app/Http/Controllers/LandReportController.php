244a899b495a97d41e9d6eff4fc4a4ece36d047a
Indumini Ranasinghe <20205283@student.curtin.edu.au>
2020-10-01 00:18:24 +0530

    Land Report Gen/installed, controller created

---
+++ app/Http/Controllers/LandReportController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+
+class LandReportController extends Controller
+{
+    //methods
+
+} //end of land report controller class
