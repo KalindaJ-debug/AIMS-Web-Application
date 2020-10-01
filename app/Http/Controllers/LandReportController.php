f64bf58f5c12bf35dfe166f5a37a14abda80133d
Indumini Ranasinghe <20205283@student.curtin.edu.au>
2020-10-01 11:51:29 +0530

    land report gen/export to pdf created

--- app/Http/Controllers/LandReportController.php
+++ app/Http/Controllers/LandReportController.php
@@ -9,5 +9,8 @@ use PDF;
 class LandReportController extends Controller
 {
     //methods
+    public function exportToPDF(){
+        
+    }//end of method
 
 } //end of land report controller class
