ea8bb91ea6ee168b0a9c905f3dd6d89fe0ede4fd
Indumini Ranasinghe <20205283@student.curtin.edu.au>
2020-10-01 15:56:02 +0530

    land report gen/generate header

--- app/Http/Controllers/LandReportController.php
+++ app/Http/Controllers/LandReportController.php
@@ -19,7 +19,6 @@ class LandReportController extends Controller
         $farmer = Farmer::where('id', $fid)->first(); //farmer name
         $landRecords = Land::with('provinces', 'districts')->where('farmer_id', $fid)->paginate(20);
 
-        // dd($landRecords);
         //convert to html view for pdf
         $htmlStream = '
         <div class="col-6">
@@ -84,9 +83,36 @@ class LandReportController extends Controller
     //export to pdf - single land record
     public function exportLandRecordPDF($lid){
         $land = Land::find($lid);
-        dd($lid);
-
+        $farmer = Farmer::where('id', $land->farmer_id)->first();
         
+        //html stream for pdf
+        $landHTMLStream = '
+        <div class="col-6">
+        <div style="max-width:100%;background-color:#08260E;border:none;">
+        <div class="row no-gutters">
+          <div class="col-md-4">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body text-center" style="padding:30px;color:white;">
+              <h2 class="card-title" style="margin-left:20px;">Agriculture Information Management System | AIMS </h2>
+              <p class="card-text" style="margin-left:400px;">Department of Agriculture</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
+
+      <br>
+      <h3 style="margin-left:250px;">Farmer Land Information</h3>
+      <p style="margin-left:50px;">Full Name: '.$farmer->firstName.'  '.$farmer->lastName.' </p>
+      <hr>
+        ';
+
+        //stream pdf
+        $pdf = App::make('dompdf.wrapper');
+        $pdf->loadHTML($landHTMLStream);
+        return $pdf->stream();
+
     }//end of method
 
 } //end of land report controller class
