865f8f31182d70cbe027a5697e19381927042fe5
23hirantha <20208253@student.curtin.edu.au>
2020-10-01 20:40:24 +0530

    pdf view update

--- resources/views/PDF/harvest.blade.php
+++ resources/views/PDF/harvest.blade.php
@@ -2,19 +2,26 @@
 <html>
 <head>
     <title>Harvest Details in Malabe Region</title>
+    <style type="text/css">
+    table{
+        width:100%;
+        margin: 0 auto;
+        border:1px solid;
+    }
+    </style>
 </head>
 <body>
 <h1>Harvest Details in Malabe Region</h1>
 <table>
     <thead>
-    
+    <tr>
         <th>ID</td>
         <th>Farmer ID</th>
         <th>Season</th>
         <th>Harvest Date</th>
         <th>Harvested Amount</th>
         <th>Cultivated Land</th>
-    
+    </tr>
     </thead>
     <tbody>
      @foreach($harvests as $key => $harvest)   
