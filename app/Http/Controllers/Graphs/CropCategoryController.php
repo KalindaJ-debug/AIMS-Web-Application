4511e04ce04d913374af75f7aec8143323549bf0
Rishikeish <20205306@student.curtin.edu.au>
2020-10-22 11:32:48 +0530

    Created crop category visualization controller

---
+++ app/Http/Controllers/Graphs/CropCategoryController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Graphs;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CropCategoryController extends Controller
+{
+    //
+}
