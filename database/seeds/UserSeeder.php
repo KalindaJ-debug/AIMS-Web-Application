9b5c4f786529e8bb88533e0a7bf94ed5570107ed
Indumini Ranasinghe <20205283@student.curtin.edu.au>
2020-10-26 01:37:37 +0530

    navbar/integration completed

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -14,12 +14,30 @@ class UserSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            'name' => 'Rishikeish',
-            'lastname' => 'Nandakumar',
-            'username' => 'rishi',
-            'email' => 'rishi@gmail.com',
-            'password' => Hash::make('password'),
-            'role' => 'Admin',
+            [
+                'name' => 'Rishikeish',
+                'lastname' => 'Nandakumar',
+                'username' => 'rishi',
+                'email' => 'rishi@gmail.com',
+                'password' => Hash::make('password'),
+                'role' => 'Admin',
+            ],
+            [
+                'name' => 'Indumini',
+                'lastname' => 'Ranasinghe',
+                'username' => 'ama',
+                'email' => 'indumini.ama@gmail.com',
+                'password' => Hash::make('password'),
+                'role' => 'FO',
+            ],
+            [
+                'name' => 'Divyargavi',
+                'lastname' => 'Gunaseelan',
+                'username' => 'ragavi',
+                'email' => 'dragavi404@gmail.com',
+                'password' => Hash::make('password'),
+                'role' => 'AI',
+            ]
             
         ]);
     }
--- resources/views/cropDetails.blade.php
+++ resources/views/cropDetails.blade.php
@@ -8,9 +8,9 @@
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
   <script src="https://kit.fontawesome.com/a076d05399.js"></script>
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  {{-- <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-  <!--Font css Link-->
+  <!--Font css Link--> --}}
   <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400&display=swap" rel="stylesheet">
 
   <title>Cultivation</title>
@@ -31,7 +31,8 @@
 <!-- nav bar begins -->
 @include('layouts.navbar')
 <!-- nav bar ends -->
-
+<br> <br>
+<h1 style="margin-left:100px;">Enter Cultivation Details</h1> <br> <br>
 <div class="container">
 <form method="post" name="form1" action="/cropDetails" enctype="multipart/form-data">
         {{ csrf_field() }}
@@ -188,6 +189,7 @@
     });
   </script>
 <!-- function to validate only decimal numbers are allowed in harvest amount field-->
+
 
 <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 <link rel="stylesheet" href="/resources/demos/style.css">
